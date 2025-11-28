c9ecd346a8ce768d3eca3e48a3c2976238ac5296
vadimgalstyan <galstyanvadim43@gmail.com>
2025-11-28 15:31:15 +0400

    Add project files.

---
+++ Models/Filter.cs
@@ -0,0 +1,46 @@
+﻿namespace CryptocoinsFilter.Models
+{
+    public class Filter
+    {
+        // Exchanges that can be choosed
+        public bool? BinanceSpot { get; set; }
+        public bool? BinanceFutures { get; set; }
+
+        public bool? BybitSpot { get; set; }
+        public bool? BybitFutures { get; set; }
+
+        public bool? BitgetSpot { get; set; }
+        public bool? BitgetFutures { get; set; }
+
+        public bool?  GateSpot { get; set; }
+        public bool? GateFutures { get; set; }
+
+        public bool? MexcSpot { get; set; }
+        public bool? MexcFutures { get; set; }
+
+        //NOT SUPPORTED EXCHANGES///
+        public bool KucoinSpot { get; set; }
+        public bool KucoinFutures { get; set; }
+
+        public bool BingxSpot { get; set; }
+        public bool BingxFutures { get; set; }
+
+        public bool LbankSpot { get; set; }
+        public bool LbankFutures { get; set; }
+
+        public bool HtxSpot { get; set; }
+        public bool HtxFutures { get; set; }
+
+        public bool OkxSpot { get; set; }
+        public bool OkxFutures { get; set; }
+
+        public bool HyperliquidSpot { get; set; }
+        public bool HyperliquidFutures { get; set; }
+
+        public bool BitmartSpot { get; set; }
+        public bool BitmartFutures { get; set; }
+
+        // Result list
+        public List<string> FilteredCoins { get; set; } = new List<string>();
+    }
+}
